824d64295dd00d8e3740bdd0fae7cb1c9900cc7b
Siddharth <greatsid2003@gmail.com>
2022-01-31 18:35:01 +0530

    nupur and aditya assigned

---
+++ Frontend/ecomm-frontend/src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return <div>
+      {/* Aditya add your code here  */}
+  </div>;
+}
+
+export default Header;
---
+++ Frontend/ecomm-frontend/src/Product/Productpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Productpage.css'
+function Productpage() {
+  return <div>
+      {/* Nupur add code here  */}
+  </div>;
+}
+
+export default Productpage;
