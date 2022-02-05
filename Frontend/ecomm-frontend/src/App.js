2ad66613f9f07f26c2ee3655752f8694d35a7685
Aditya <aditya261862@gmail.com>
2022-02-05 20:22:42 +0530

    Header

--- Frontend/ecomm-frontend/src/App.js
+++ Frontend/ecomm-frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header  from './Header/Header';
 import LoginSignup from './LoginSignup/LoginSignup';
+import Footer from './Footer/Footer'
 function App() {
   return (
     <div className="App">
       <Header/>
       <LoginSignup/>
+      <Footer/>
     </div>
   );
 }
