26056b88a9a3b02953b388bee166eb5818f4c368
Sagar Nishad <72318640+sagar-nishad@users.noreply.github.com>
2022-02-10 19:08:53 +0530

    Update Cards.js

--- Frontend/ecomm-frontend/src/ProductCards/Cards.js
+++ Frontend/ecomm-frontend/src/ProductCards/Cards.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card1 from "./Card1";
 import Card2 from "./Card2";
+import axios from "axios";
 // import Card2 from './Card2'
 
 import "./Cards.css";
 
+
 function Cards() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/product/new").then((res) => {
+      setProducts(res.data);
+      console.log("products >>>",res.data);
+    });
+  }, []);
   return (
     <div className="cards">
-      <Card1 />
-      <Card1 />
-      <Card2 />
-      <Card2 />
-      <Card1 />
-      <Card2 />
+      
+      {products&& products.map(product => <Card1 key={product._id} name={product.name} img={product.productImage}/>)}
     </div>
   );
 }
